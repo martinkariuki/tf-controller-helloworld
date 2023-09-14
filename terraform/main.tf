37ad92f725e52b203226f115fc23b4b3e6762762
martinkariuki <35515749+martinkariuki@users.noreply.github.com>
2023-09-14 10:46:40 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,15 @@ variable "subject" {
 output "hello_world" {
   value = "hey hey ya, ${var.subject}!"
 }
+
+data "azurerm_resource_group" "example" {
+  name     = "argo-1"
+  location = "Canada Central"
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
